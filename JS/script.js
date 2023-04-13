7459a6b207ec98500e2e264e908d1fdc87830426
JulianRG <julianrg1512@gmail.com>
2023-04-13 16:14:58 -0500

    Se agrega el proyecto Portafolio Personal

---
+++ JS/script.js
@@ -0,0 +1,54 @@
+(function() {
+    $('.btn').click(function() {
+        $(this).toggleClass('active');
+        return $('.box') .toggleClass('open');
+    });
+    
+}).call(this);
+
+(function() {
+    $('.btn').click(function() {
+        $(this).toggleClass('active');
+        return $('.box1') .toggleClass('open');
+    });
+    
+}).call(this);
+
+
+
+(function() {
+    $('.btn2').click(function() {
+        $(this).toggleClass('active');
+        return $('.box2').toggleClass('open2');
+    });
+    
+}).call(this);
+
+
+
+(function() {
+    $('.btn3').click(function() {
+        $(this).toggleClass('active');
+        return $('.box3').toggleClass('open3');
+    });
+    
+}).call(this);
+
+
+
+(function() {
+    $('.btn4').click(function() {
+        $(this).toggleClass('active');
+        return $('.box4').toggleClass('open4');
+    });
+    
+}).call(this);
+
+(function() {
+    $('.btn4').click(function() {
+        $(this).toggleClass('active');
+        return $('.box4_1').toggleClass('open4');
+    });
+    
+}).call(this);
+
